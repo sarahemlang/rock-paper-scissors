801adc6ccc5a975673500fd3fe242a8f1897fe4d
Sarah <sarahemlang@gmail.com>
2022-01-08 13:00:37 -0500

    Added logic for deciding winner

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const computerChoice = ["Rock", "Paper", "Scissors"]
-console.group(computerChoice)
+
+let playerSelection = prompt("Rock, Paper or Scissors?")
 
 function computerPlay() {
-    let choice = Math.floor(Math.random() * 4);
-    console.log(computerChoice[choice])
+    let pickRandom = Math.floor(Math.random() * 3);
+    return computerSelection = computerChoice[pickRandom];
 }
 computerPlay()
+
+console.log(computerSelection)
+console.log(playerSelection)
+
+function playRound() {
+    if (computerSelection === playerSelection) {
+        console.log("Draw!")
+    }
+    else if (computerSelection === "Rock" && playerSelection === "Scissors" || computerSelection === "Paper" && playerSelection === "Rock" || computerSelection === "Scissors" && playerSelection === "Paper") {
+        console.log("You lose")
+    }
+    else {
+        console.log("You win!")
+    }
+}
+playRound()
