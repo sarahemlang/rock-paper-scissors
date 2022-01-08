64bd134d4a552bdc4154e5bfdc937b4e5c25c44d
Sarah <sarahemlang@gmail.com>
2022-01-08 13:25:08 -0500

    Added comments explaining, reworked game logic

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-const computerChoice = ["Rock", "Paper", "Scissors"]
-
-let playerSelection = prompt("Rock, Paper or Scissors?")
-
+//this is the array the computer picks from
+const computerChoice = ["ROCK", "PAPER", "SCISSORS"]
+//this function generates a random index and uses it to pick an item from the computerChoice array 
 function computerPlay() {
     let pickRandom = Math.floor(Math.random() * 3);
     return computerSelection = computerChoice[pickRandom];
 }
+//I don't know how to get my functions to run other than calling them in the code so for now this is for testing purposes
 computerPlay()
 
+//prompts for a player selection and capitalizes it for comparision
+let playerSelection = prompt("Rock, Paper or Scissors?")
+playerSelection = playerSelection.toUpperCase();
+
+//this just prints choices so I can test and see
 console.log(computerSelection)
 console.log(playerSelection)
 
-function playRound() {
+//this function plays a round - compares computerSelection and playerSelection and console.log's who wins
+function playRound(computerSelection, playerSelection) {
     if (computerSelection === playerSelection) {
         console.log("Draw!")
     }
-    else if (computerSelection === "Rock" && playerSelection === "Scissors" || computerSelection === "Paper" && playerSelection === "Rock" || computerSelection === "Scissors" && playerSelection === "Paper") {
+    else if (computerSelection === "ROCK" && playerSelection === "SCISSORS" || computerSelection === "PAPER" && playerSelection === "ROCK" || computerSelection === "SCISSORS" && playerSelection === "PAPER") {
         console.log("You lose")
     }
     else {
         console.log("You win!")
     }
 }
+//once again don't know how to get my functions to just run
 playRound()
